bb25b0c42b8e88cf2df5cd839f64c30adf72b2df
qywi <teslagamnesla.123456789@gmail.com>
2024-09-21 17:14:04 +0300

    feat: fix progress bar

--- script.js
+++ script.js
@@ -9,6 +9,7 @@ const time_bar = document.querySelector(".time-bar");
 const time = document.querySelector(".time");
 const progres_bar = document.querySelector(".progres-bar");
 const progres = document.querySelector(".progres");
+const sliderThumb = document.querySelector(".slider-thumb");
 const download = document.querySelector(".download");
 const audio = document.querySelector(".audio");
 
@@ -16,55 +17,56 @@ const songs = ["It's Goind Down Now", "Take Me Out", "Just The Two Of Us"];
 const creators = ["Persona 3 Reload", "Franz Ferdinand", "Sagan girl"];
 const imgArr = ["assets/music_img/persona3.jpg", "assets/music_img/Take_Me_Out.jpg", "assets/music_img/just_the_two_of_us.jpg"];
 const songsMp3 = ["assets/music/It's_Goind_Down_Now.opus", "assets/music/Take_Me_Out.opus", "assets/music/Just_The_Two_Of_Us.opus"];
-const timeArr = ["3:06", "3:57", "4:19"]
-const color = ["#01aefb", "#f39904", "#ae3850"]
+const timeArr = ["3:06", "3:57", "4:19"];
+const color = ["#01aefb", "#f39904", "#ae3850"];
 
 let songIndex = 0;
+let isDragging = false;
+let wasPlaying = false;
 
 function loadSong(song, artist, imgg, mp3, time, color) {
     song_artist.innerHTML = artist;
     song_title.innerHTML = song;
-    audio.src = `${mp3}`;
+    audio.src = mp3;
     img.style.backgroundImage = `url(${imgg})`;
     background.style.backgroundImage = `url(${imgg})`;
     time_bar.innerHTML = time;
-    time_bar.style.color  = color;
-    song_artist.style.color  = color;
+    time_bar.style.color = color;
+    song_artist.style.color = color;
+    sliderThumb.style.backgroundColor = color;
 }
-
-backward.addEventListener('click', () => {
-    if (songIndex === 0) {
-        songIndex = songs.length - 1;
-    }
-    else songIndex--;
-
-    loadSong(songs[songIndex], creators[songIndex], imgArr[songIndex], songsMp3[songIndex], timeArr[songIndex], color[songIndex])
-    playSong()
-    return songIndex;
-})
-
-forward.addEventListener('click', () => {
-    if (songIndex === songs.length - 1) {
-        songIndex = 0;
-    }
-    else songIndex++;
-
-    loadSong(songs[songIndex], creators[songIndex], imgArr[songIndex], songsMp3[songIndex], timeArr[songIndex], color[songIndex])
-    playSong()
-    return songIndex;
-})
 
 function playSong() {
     play.classList.add('playing');
     audio.play();
-    play.src = 'assets/icon/pause.png'
+    play.src = 'assets/icon/pause.png';
 }
 
 function pauseSong() {
     play.classList.remove('playing');
     audio.pause();
-    play.src = 'assets/icon/play.png'
+    play.src = 'assets/icon/play.png';
 }
+
+backward.addEventListener('click', () => {
+    if (songIndex === 0) {
+        songIndex = songs.length - 1;
+    } else {
+        songIndex--;
+    }
+    loadSong(songs[songIndex], creators[songIndex], imgArr[songIndex], songsMp3[songIndex], timeArr[songIndex], color[songIndex]);
+    playSong();
+});
+
+forward.addEventListener('click', () => {
+    if (songIndex === songs.length - 1) {
+        songIndex = 0;
+    } else {
+        songIndex++;
+    }
+    loadSong(songs[songIndex], creators[songIndex], imgArr[songIndex], songsMp3[songIndex], timeArr[songIndex], color[songIndex]);
+    playSong();
+});
 
 play.addEventListener('click', () => {
     const isPlaying = play.classList.contains('playing');
@@ -73,24 +75,79 @@ play.addEventListener('click', () => {
     } else {
         playSong();
     }
-})
+});
 
-function updateProgress() {
-    const duration = audio.duration;
-    const currentTime = audio.currentTime;
-
-    if (duration > 0) {
-        const progressPercent = (currentTime / duration) * 100;
-        progres.style.width = `${progressPercent}%`;
-    }
+function updateProgressBar(currentTime, duration) {
+    const progressPercent = (currentTime / duration) * 100;
+    progres.style.width = `${progressPercent}%`;
+    sliderThumb.style.left = `${progressPercent}%`;
 }
 
 audio.addEventListener('timeupdate', () => {
-    updateProgress();
-
+    if (!isDragging) {
+        updateProgressBar(audio.currentTime, audio.duration);
+    }
     if (time) {
         time.textContent = formatTime(audio.currentTime);
     }
+});
+
+progres_bar.addEventListener('click', (e) => {
+    const width = progres_bar.clientWidth;
+    const clickX = e.offsetX;
+    const duration = audio.duration;
+
+    audio.currentTime = (clickX / width) * duration;
+});
+
+sliderThumb.addEventListener('mousedown', () => {
+    isDragging = true;
+    wasPlaying = !audio.paused;
+    pauseSong();
+});
+
+document.addEventListener('mousemove', (e) => {
+    if (isDragging) {
+        const rect = progres_bar.getBoundingClientRect();
+        const width = progres_bar.clientWidth;
+        let clickX = e.clientX - rect.left;
+
+        if (clickX < 0) clickX = 0;
+        if (clickX > width) clickX = width;
+
+        const duration = audio.duration;
+        const newTime = (clickX / width) * duration;
+
+        updateProgressBar(newTime, duration);
+        audio.currentTime = newTime;
+    }
+});
+
+document.addEventListener('mouseup', () => {
+    if (isDragging) {
+        isDragging = false;
+        if (wasPlaying) {
+            playSong();
+        }
+    }
+});
+
+audio.addEventListener('ended', () => {
+    if (songIndex === songs.length - 1) {
+        songIndex = 0;
+    } else {
+        songIndex++;
+    }
+    loadSong(songs[songIndex], creators[songIndex], imgArr[songIndex], songsMp3[songIndex], timeArr[songIndex], color[songIndex]);
+    playSong();
+});
+
+download.addEventListener('click', () => {
+    const songSrc = audio.src;
+    const link = document.createElement('a');
+    link.href = songSrc;
+    link.download = songSrc.split('/').pop();
+    link.click();
 });
 
 function formatTime(seconds) {
@@ -99,34 +156,4 @@ function formatTime(seconds) {
     return `${minutes}:${secondsLeft < 10 ? '0' : ''}${secondsLeft}`;
 }
 
-function setProgress(e) {
-    const width = this.clientWidth;
-    const clickX = e.offsetX
-    const duration = audio.duration;
-
-    audio.currentTime = (clickX / width) * duration;
-}
-
-progres_bar.addEventListener('click', setProgress);
-
-audio.addEventListener('ended', () => {
-    if (songIndex === songs.length - 1) {
-        songIndex = 0;
-    }
-    else songIndex++;
-
-    loadSong(songs[songIndex], creators[songIndex], imgArr[songIndex], songsMp3[songIndex], timeArr[songIndex], color[songIndex])
-    playSong()
-    return songIndex;
-})
-
-download.addEventListener('click', () => {
-    const songSrc = audio.src;
-    const link = document.createElement('a'); 
-
-    link.href = songSrc; 
-    link.download = songSrc.split('/').pop(); 
-    link.click();
-});
-
-loadSong(songs[songIndex], creators[songIndex], imgArr[songIndex], songsMp3[songIndex], timeArr[songIndex], color[songIndex])
+loadSong(songs[songIndex], creators[songIndex], imgArr[songIndex], songsMp3[songIndex], timeArr[songIndex], color[songIndex]);
